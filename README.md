d5e21f861ff19de6fed1446297fbf2648926dda1
lishengzxc <eric@lishengcn.cn>
2015-05-24 16:12:44 +0800

    Update 2015-05-24T08:12:03.819Z

--- README.md
+++
@@ -1,2 +0,0 @@
-# 2333diary
-This is my 2333diary. I'll put some tips in it.
--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var deploy = require('gulp-gh-pages');
-// var options = {};
-gulp.task('deploy', function() {
-  return gulp.src('./preview/**/*')
-    .pipe(deploy());
-});
-gulp.task('default', ['deploy']);
